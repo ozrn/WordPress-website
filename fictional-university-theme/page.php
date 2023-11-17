ec1a813f04dec2c4b77c6ea3bb2175b26b8d8648
kevser özeren <ozerenkevser92@gmail.com>
2023-11-17 23:34:29 +1300

    Add global header&footer to each post and page

--- fictional-university-theme/page.php
+++ fictional-university-theme/page.php
@@ -1,10 +1,14 @@
 <?php
 
+get_header();
+
 while (have_posts()) {
     the_post(); ?>
     <h2>This is a test page!</h2>
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
 <?php }
+
+get_footer();
 
 ?>
