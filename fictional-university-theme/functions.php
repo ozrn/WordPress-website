a95223b55cdbb438b511cdb37127612680d71a30
kevser özeren <ozerenkevser92@gmail.com>
2023-12-01 23:19:21 +1300

    Handle slideshow behaviour loading JS file

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -2,6 +2,7 @@
 
 function uni_files()
 {
+    wp_enqueue_script('main-uni-js', get_theme_file_uri('/build/index.js'), NULL, '1.0', true);
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
     wp_enqueue_style('uni_main_styles', get_theme_file_uri('/build/style-index.css'));
