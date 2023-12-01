d52685fef00b9278d997fc7de65135435fa48ac6
kevser özeren <ozerenkevser92@gmail.com>
2023-12-01 22:04:18 +1300

    Load external CSS files using WP function

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -2,6 +2,8 @@
 
 function uni_files()
 {
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
     wp_enqueue_style('uni_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('uni_extra_styles', get_theme_file_uri('/build/index.css'));
 }
