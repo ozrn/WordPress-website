06a49ab5d7d06609f7debc9664ccf01f82ec61d6
kevser özeren <ozerenkevser92@gmail.com>
2023-12-04 19:02:15 +1300

    Add new action to generate title for each screen

--- fictional-university-theme/functions.php
+++ fictional-university-theme/functions.php
@@ -10,3 +10,9 @@ function uni_files()
 }
 
 add_action('wp_enqueue_scripts', 'uni_files');
+
+function uni_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'uni_features');
