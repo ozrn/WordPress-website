f3db93ab7b4d179e21fc021d99a9ecbed9fa7fc5
kevser özeren <ozerenkevser92@gmail.com>
2023-12-06 18:50:44 +1300

    Display meta box only if current page is child

--- fictional-university-theme/page.php
+++ fictional-university-theme/page.php
@@ -4,23 +4,30 @@ get_header();
 
 while (have_posts()) {
     the_post(); ?>
-    
-    <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+
+<div class="page-banner">
+    <div class="page-banner__bg-image"
+        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title(); ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
-
-  <div class="container container--narrow page-section">
-
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span></p>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+            <p>DONT FORGET TO REPLACE ME LATER</p>
+        </div>
     </div>
-    
+</div>
+
+<div class="container container--narrow page-section">
+    <?php
+      if (wp_get_post_parent_id(get_the_ID())) { ?>
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About
+                Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+    </div>
+    <?php }
+    ?>
+
+
+
     <!--
     <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
@@ -32,10 +39,10 @@ while (have_posts()) {
     -->
 
     <div class="generic-content">
-      <?php the_content(); ?>
+        <?php the_content(); ?>
     </div>
 
-  </div>
+</div>
 <?php }
 
 get_footer();
